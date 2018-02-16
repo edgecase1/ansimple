2c97d6b5e135a444281141f8521dc6417673789c
Reinhard <catmin@gmx.at>
2018-02-17 00:17:02 +0100

    can check and install

--- ansimple.py
+++ ansimple.py
@@ -1,9 +1,8 @@
-
+#!/usr/bin/env python3
 
 import json
 import logging
-
-
+import sys
 
 
 import subprocess
@@ -19,18 +18,38 @@ class AptHandler:
     def _check_env(self):
         pass
 
-    def apply(self, item):
-        data = item[self.provider]
+    def _install(self, package):
         if os.geteuid() != 0:
             raise Exception("not effective user ID 0! run with sudo")
 
-        cmd = [ "apt-get", "install", "-y", data["name"] ]
+        cmd = [ "apt-get", "install", "-y", package ]
         logger.debug("running '{0}'".format(cmd))
         process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
         logger.debug(process.stdout.read())
         process.communicate()
         if process.returncode != 0: raise Exception("error running process {0} - rc={1}".format(cmd[0], process.returncode))
 
+        return
+
+    def _is_installed(self, package):
+        cmd = [ "dpkg", "-s", package ]
+        logger.debug("running '{0}'".format(cmd))
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.communicate()
+        if process.returncode == 0: 
+            return True 
+        else:
+            return False
+
+
+    def apply(self, item):
+        data = item[self.provider]
+        package = data["name"]
+        if not self._is_installed(package):
+            logger.info("installing package {0}.".format(package))
+            self._install(package)
+        else:
+            logger.debug("package {0} already installed.".format(package))
         return
 
     def __repr__(self):
@@ -54,6 +73,12 @@ class ItemHandlerFactory:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger("main")
+
+    playbook_path = sys.argv[1]
+    if not os.path.exists(playbook_path):
+        print("playbook '{0}' not found.".format(playbook_path))
+        sys.exit(1)
+
     playbook = []
     with open("playbook.yml", "r") as f:
         playbook = json.load(f)
@@ -69,3 +94,4 @@ if __name__ == "__main__":
             logger.error("error running handler {1} with data {0}". format(item, handler))
             logger.error(e)
     logger.debug("done") 
+
