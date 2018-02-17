bfa7984ec28f6d7878ed9c88bbbe2d22c5a6b7df
Reinhard <catmin@gmx.at>
2018-02-17 12:08:02 +0100

    added testsuite and file provider

--- ansimple.py
+++ ansimple.py
@@ -12,20 +12,17 @@ class AptHandler:
     provider = "apt"
 
     def __init__(self):
-        self._check_env()
+        self.logger = logging.getLogger("main")
         return
-
-    def _check_env(self):
-        pass
 
     def _install(self, package):
         if os.geteuid() != 0:
             raise Exception("not effective user ID 0! run with sudo")
 
         cmd = [ "apt-get", "install", "-y", package ]
-        logger.debug("running '{0}'".format(cmd))
+        self.logger.debug("running '{0}'".format(cmd))
         process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
-        logger.debug(process.stdout.read())
+        self.logger.debug(process.stdout.read())
         process.communicate()
         if process.returncode != 0: raise Exception("error running process {0} - rc={1}".format(cmd[0], process.returncode))
 
@@ -33,7 +30,7 @@ class AptHandler:
 
     def _is_installed(self, package):
         cmd = [ "dpkg", "-s", package ]
-        logger.debug("running '{0}'".format(cmd))
+        self.logger.debug("running '{0}'".format(cmd))
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process.communicate()
         if process.returncode == 0: 
@@ -46,10 +43,101 @@ class AptHandler:
         data = item[self.provider]
         package = data["name"]
         if not self._is_installed(package):
-            logger.info("installing package {0}.".format(package))
+            self.logger.info("installing package {0}.".format(package))
             self._install(package)
         else:
-            logger.debug("package {0} already installed.".format(package))
+            self.logger.debug("package {0} already installed.".format(package))
+        return
+
+    def __repr__(self):
+        return self.provider
+
+import os
+import stat
+from pwd import getpwuid, getpwnam
+from grp import getgrgid, getgrnam
+from string import Template
+class FileHandler():
+    provider = "file"
+
+    def __init__(self):
+        self.logger = logging.getLogger("main")
+        return
+    
+    def apply(self, item):
+        data = item[self.provider]
+        file_path = data["path"]
+
+        set_mode = False
+        if "mode" in data:
+            if isinstance(data["mode"], int):
+                file_mode = data["mode"]
+                # TODO check range
+            #elif g+x u+s
+            else:
+                raise Exception("invalid mode")
+            set_mode = True
+
+        change_owner = False
+        if "owner" in data: 
+            # get user details by username or uid
+            if isinstance(data["owner"], int):
+                file_owner = getpwuid(data["owner"])
+            elif isinstance(data["owner"], str):
+                file_owner = getpwnam(data["owner"])
+            else:
+                raise Exception("invalid data type for user")
+            change_owner = True
+
+        change_group = False
+        if "group" in data: 
+            # get group details by groupname or gid
+            if isinstance(data["group"], int):
+                file_group = getpwgid(data["group"])
+            elif isinstance(data["owner"], str):
+                file_group = getgrnam(data["group"])
+            else:
+                raise Exception("invalid data type for user")
+            change_group = True
+
+        # content
+        write_contents_to_file = False
+        if "content" in data:
+            content = data["content"]
+            write_contents_to_file = True
+        elif "template" in data:
+            template = data["template"]
+            if not os.path.exists(template): raise Exception("template '{0}' not found".format(template))
+            self.logger.debug("loading template '{0}'".format(template))
+            with open(template, "r") as template_file:
+                template = Template(template_file.read())
+                if "vars" in data:
+                    template_vars = data["vars"]
+                else:
+                    template_vars = {}
+                content = template.substitute(template_vars)
+            write_contents_to_file = True
+        if write_contents_to_file:
+            self.logger.debug("writing to file '{0}'".format(file_path))
+            with open(file_path, "w") as outfile:
+                outfile.write(content)
+
+        # the file has to exist at this stage
+        if not os.path.exists(file_path): raise Exception("file '{0}' not found.".format(file_path))
+
+        # set mode
+        if set_mode:
+            self.logger.debug("setting mode to {0}".format(file_mode))
+            os.chmod(file_path, file_mode) # PermissionError
+
+        # set owner and group
+        if change_owner:
+            self.logger.debug("setting owner of '{1}' to '{0}'".format(file_owner.pw_name, file_path))
+            os.chown(file_path, file_owner.pw_uid, -1) # change only the owner
+        if change_group:
+            self.logger.debug("setting group of '{1}' to '{0}'".format(file_group.gr_name, file_path))
+            os.chown(file_path, -1, file_group.gr_gid) # change only the group
+
         return
 
     def __repr__(self):
@@ -59,6 +147,9 @@ class ItemHandlerFactory:
 
     def __init__(self):
         self.handlers = {}
+        self.add_handler(AptHandler())
+        self.add_handler(FileHandler())
+        return
 
     def add_handler(self, handler):
         self.handlers[handler.provider] = handler
@@ -70,11 +161,33 @@ class ItemHandlerFactory:
         else:
             return None
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+
+def main(playbook_path):
+    logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger("main")
 
-    # error if wrong arguments are provided
+    # parse playbook file
+    playbook = []
+    with open(playbook_path, "r") as f:
+        playbook = json.load(f)
+    logger.debug("parsed playbook:" + str(playbook))
+
+    # process playbook
+    factory = ItemHandlerFactory()     
+    for item in playbook:
+        handler = factory.create_by_type(item)
+        if not handler:
+            logger.error("no handler defined for {0}".format(item))
+            continue
+        #try:
+        handler.apply(item)
+        #except Exception as e:
+        #    logger.error("error running handler {1} with data {0}". format(item, handler))
+        #    logger.error(e)
+    logger.debug("done") 
+    return
+
+if __name__ == "__main__":
     if not len(sys.argv) != 1:
         print("usage: ansimple.py ./playbook.yml")
         sys.exit(1)
@@ -82,22 +195,5 @@ if __name__ == "__main__":
     if not os.path.exists(playbook_path):
         print("playbook '{0}' not found.".format(playbook_path))
         sys.exit(1)
-
-    # parse playbook file
-    playbook = []
-    with open("playbook.yml", "r") as f:
-        playbook = json.load(f)
-    logger.debug("parsed playbook:" + str(playbook))
-
-    # process playbook
-    factory = ItemHandlerFactory()     
-    factory.add_handler(AptHandler())
-    for item in playbook:
-        handler = factory.create_by_type(item)
-        try:
-            handler.apply(item)
-        except Exception as e:
-            logger.error("error running handler {1} with data {0}". format(item, handler))
-            logger.error(e)
-    logger.debug("done") 
+    main(playbook_path)
 
---
+++ testsuite.py
@@ -0,0 +1,33 @@
+
+import unittest
+import json
+import ansimple
+
+class TestFiles(unittest.TestCase):
+
+    def create_file(self, content):
+        content_json = json.dumps(content)
+        testfile_path = "testfile"
+        with open(testfile_path, "w") as f:
+            f.write(content_json)
+        return testfile_path
+
+    def test_install_apt_package(self):
+        content = [{ "apt": { "name": "vim" } }]
+        content_json = json.dumps(content)
+        playbook = self.create_file(content)
+        ansimple.main(playbook)
+        return
+
+    def test_create_file_with_mode(self):
+        content = [{ "file": { "path": "/tmp/xxx-avgowl-avgowl-666", "mode": 666, "owner": "avgowl", "group": "avgowl", "content": "" } }]
+        playbook = self.create_file(content)
+        ansimple.main(playbook)
+        return
+
+    def test_create_file_with_template(self):
+        content = [{ "file": { "path": "/tmp/template-out", "template": "xxx", "vars": { "name": "Reinhard"} } }]
+        playbook = self.create_file(content)
+        ansimple.main(playbook)
+        return
+
